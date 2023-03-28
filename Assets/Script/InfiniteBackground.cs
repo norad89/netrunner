87b3011b36252bb2384040e1a42cd98de28d8867
Gianpaolo <gianpaolo.raimondi@gmail.com>
2023-03-28 19:48:30 +0200

    add looping background

--- Assets/Script/InfiniteBackground.cs
+++ Assets/Script/InfiniteBackground.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class InfiniteBackground : MonoBehaviour
 {
+    public GameObject backGroundPrefab;
+    private GameObject player;
+    private float backgroundWidth = 540;
+    private bool backgroundSpawned = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector3 backgroundSpawnPos = new Vector3(transform.position.x + backgroundWidth, transform.position.y, transform.position.z);
+
+        if (player.transform.position.x > transform.position.x && !backgroundSpawned)
+        {
+            GameObject newBackground = Instantiate(backGroundPrefab, backgroundSpawnPos, backGroundPrefab.transform.rotation);
+            backgroundSpawned = true;
+        }
+        else if (player.transform.position.x > transform.position.x + backgroundWidth)
+        {
+            Destroy(gameObject);
+        }
     }
+
 }
