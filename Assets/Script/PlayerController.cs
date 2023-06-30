88eac9291625dc00fa57086a8e7fa17032a5fea3
Gianpaolo <gianpaolo.raimondi@gmail.com>
2023-06-30 10:48:34 +0200

    working player selection

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public GameObject cubePlayer;
+    public GameObject spherePlayer;
     public float jumpForce = 1200;
     public float jumpGravityModifier = 4f;
     public float fallGravityModifier = 3f;
     public bool canDoubleJump = true;
     public bool isOnGround = true;
     private GameManager gameManager;
+    private BoxCollider boxCollider;
+    private SphereCollider sphereCollider;
 
     void Start()
     {
         gameManager = GameManager.Instance;
-
+        boxCollider = GetComponent<BoxCollider>();
+        sphereCollider = GetComponent<SphereCollider>();
         playerRb = GetComponent<Rigidbody>();
         Physics.gravity *= jumpGravityModifier;
     }
 
     void Update()
     {
+        if (gameManager.playerType == "cube")
+        {
+            boxCollider.enabled = true;
+            sphereCollider.enabled = false;
+            cubePlayer.SetActive(true);
+            spherePlayer.SetActive(false);
+        }
+        else if (gameManager.playerType == "sphere")
+        {
+            sphereCollider.enabled = true;
+            boxCollider.enabled = false;
+            cubePlayer.SetActive(false);
+            spherePlayer.SetActive(true);
+        }
+
         if ((Input.GetKeyDown(KeyCode.Space)) && (isOnGround || (canDoubleJump && gameManager.powerUpCount != 0)) && gameManager.isGameActive)
         {
             if (!isOnGround)
