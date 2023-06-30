a6e69950a7a6a727e083a9ecf7fa84a0a44d8aae
Gianpaolo <gianpaolo.raimondi@gmail.com>
2023-06-30 10:32:07 +0200

    fix self destruct

--- Assets/Script/SelfDestruct.cs
+++ Assets/Script/SelfDestruct.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour
 {
+    private GameManager gameManager;
     private Transform playerTransform; // Riferimento al transform del giocatore
     public float destroyDistance = 30f; // Distanza a cui distruggere l'oggetto
 
     void Start()
     {
-        GameObject player = GameObject.Find("Player");
-        playerTransform = player.transform;
+        gameManager = GameManager.Instance;
     }
 
     void Update()
     {
+        if (gameManager.player) {
+      
         // Verifica se l'oggetto si trova a sinistra del giocatore
-        if (transform.position.x < playerTransform.position.x - destroyDistance)
+        if (transform.position.x < gameManager.player.transform.position.x - destroyDistance)
         {
             // Distrugge l'oggetto
             Destroy(gameObject);
+        }
         }
     }
 }
